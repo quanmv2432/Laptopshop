0c3da1fed9d949d2b233bcbc0ca0410e80838166
quanmv2432 <113593170+quanmv2432@users.noreply.github.com>
2023-11-23 16:09:20 +0700

    Add files via upload

---
+++ src/test/java/com/laptopshop/LaptopShopApplicationTests.java
@@ -0,0 +1,38 @@
+package com.laptopshop;
+
+import com.laptopshop.repository.DonHangRepository;
+import com.laptopshop.repository.NguoiDungRepository;
+import com.laptopshop.service.DanhMucService;
+import com.laptopshop.service.NguoiDungService;
+import com.laptopshop.service.SanPhamService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaptopShopApplicationTests {
+
+	@Autowired
+	private DanhMucService dmService;
+
+	@Autowired
+	private NguoiDungService nguoiDungService;
+
+	@Autowired
+	private NguoiDungRepository ndRepo;
+
+	@Autowired
+	private DonHangRepository dhRepo;
+	
+	@Autowired
+	private SanPhamService spService;
+
+	@Test
+	public void getALlDanhMucTest() {
+		System.out.println(spService.getSanPhamByTenSanPhamForAdmin("asus",0,10).getContent().size());
+	}
+
+}
